53bb91888d7365c2688eb047d5b819327b75f954
pezhe <pezhe@list.ru>
2022-02-02 20:34:32 +0300

    Lesson 4 Initial commit

---
+++ src/test/java/ru/gb/springbootapp/SpringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
